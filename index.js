47a19f16be843f968a22d26b4735947108de2d3e
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-01-19 15:32:22 +0100

    Remove batch send

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var { ApiPromise, WsProvider } = require('@polkadot/api');
+var { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+var { cryptoWaitReady } = require('@polkadot/util-crypto');
 
 // Main function which needs to run at start
 async function main() {
+  await cryptoWaitReady();
+  const keyring = new Keyring({ type: 'sr25519' });
   // Substrate node we are connected to and listening to remarks
   const provider = new WsProvider('ws://localhost:9944');
   //const provider = new WsProvider('wss://kusama-rpc.polkadot.io/');
@@ -18,30 +21,41 @@ async function main() {
     `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
   );
 
+  // Add account with URI
+  let account = keyring.addFromUri('//Alice', { name: 'Alice default' });
+  // Add account with Polkadot JS JSON
+  // let input_json = require('./account.json');
+  // await keyring.addFromJson(input_json);
+  // await keyring.getPair(input_json.address).decodePkcs8('password');
+  // let account = keyring.getPair(input_json.address);
+
   // Subscribe to new blocks being produced, not necessarily finalized ones.
   const unsubscribe = await api.rpc.chain.subscribeNewHeads(async header => {
     await api.rpc.chain.getBlock(header.hash, async block => {
-      // Try to never crash
-      try {
         // The block number
         let blockNumber = block.block.header.number.toNumber();
         // Extrinsics in the block
-        let extrinsics = await block.block.extrinsics;
+		let extrinsics = await block.block.extrinsics;
+		// Current account nonce:
+		let accountNonce = await api.query.system.accountNonce(account.address);
 
-        console.log(`Block ${blockNumber} had ${extrinsics.length} extrinsics`);
+        console.log(`Block ${blockNumber} had ${extrinsics.length} extrinsics and Alice has nonce ${accountNonce}`);
 
-        // // Check each extrinsic in the block
-        // for (extrinsic of extrinsics) {
-        //   // This specific call index [0,1] represents `system.remark`
-        //   if (extrinsic.callIndex[0] == 0 && extrinsic.callIndex[1] == 1) {
-
-        //   }
-        // }
-      } catch (e) {
-        console.log(e);
-      }
+        let promises = [];
+        let tx_batch_size = 400;
+        // Create transactions
+        for (let i = 0; i < tx_batch_size; i += 1) {
+          let txNonce = parseInt(accountNonce) + parseInt(i);
+          promises.push(
+            api.tx.system.remark('').signAndSend(account, { nonce: txNonce })
+          );
+        }
+		Promise.all(promises)
     });
   });
 }
+
+// Suppress error messages from RPC Core
+console.error = function() {}
 
 main().catch(console.error);

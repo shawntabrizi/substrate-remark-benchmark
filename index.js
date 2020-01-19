b642d66d5355cd81d69317846afd0478342380af
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-01-19 13:46:21 +0100

    Initial frankenstein

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 80;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index', { page: 'Home' }))
+  .listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
+
+var { ApiPromise, WsProvider } = require('@polkadot/api');
+
+// Main function which needs to run at start
+async function main() {
+  // Substrate node we are connected to and listening to remarks
+  const provider = new WsProvider('ws://localhost:9944');
+  //const provider = new WsProvider('wss://kusama-rpc.polkadot.io/');
+
+  const api = await ApiPromise.create({ provider });
+
+  // Get general information about the node we are connected to
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
+  console.log(
+    `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
+  );
+
+  // Subscribe to new blocks being produced, not necessarily finalized ones.
+  const unsubscribe = await api.rpc.chain.subscribeNewHeads(async header => {
+    await api.rpc.chain.getBlock(header.hash, async block => {
+      // Try to never crash
+      try {
+        // The block number
+        let blockNumber = block.block.header.number.toNumber();
+        // Extrinsics in the block
+        let extrinsics = await block.block.extrinsics;
+
+        console.log(`Block ${blockNumber} had ${extrinsics.length} extrinsics`);
+
+        // // Check each extrinsic in the block
+        // for (extrinsic of extrinsics) {
+        //   // This specific call index [0,1] represents `system.remark`
+        //   if (extrinsic.callIndex[0] == 0 && extrinsic.callIndex[1] == 1) {
+
+        //   }
+        // }
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  });
+}
+
+main().catch(console.error);
